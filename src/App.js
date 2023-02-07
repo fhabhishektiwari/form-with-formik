7bd8a840d9623324aae3c09cbc07da2422316a6f
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-02-07 15:12:48 +0530

    added validation with error message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className='wrapper'>
         <div className='main-form'>
-          <h1>Form With Formik</h1>
+          <h1 style={{fontFamily:'verdana',color:'#CF8'}}>Form With Formik</h1>
           <SimpleForm/>
         </div>
       </div>
--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import {useFormik} from 'formik';
 
+const initialValues = {
+    name: "Abhishek",
+    email: "",
+    channel: "",
+}
+const onSubmit=values=>{
+    console.log(values);
+}
+
+const validate = values => {
+    // values.name values.email values.channel
+    // errors.name errors.email errors.channel
+    // errors.name='This field is required'
+    let errors = {}
+
+    if (!values.name) {
+        errors.name = "Required";
+    } if (!values.email) {
+        errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = "Invalid email address";
+    } if (!values.channel) {
+        errors.channel = "Required";
+    }
+    return errors;
+}
+
 const SimpleForm = () => {
     const formik=useFormik({
-        initialValues:{
-            name:"",
-            email:"",
-            channel:"",
-        },
-        onSubmit:values=>{
-            console.log(values);
-        }
+        initialValues,
+        onSubmit,
+        validate,
     })
 
-    // console.log("Form Vslues",formik.values);
+    // console.log("Form Values",formik.values);
+    console.log("Form Errors",formik.errors);
+
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
