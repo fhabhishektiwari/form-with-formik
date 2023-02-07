9fba19c659bbbf7ec804db680122af0937a54c5e
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-02-07 15:52:10 +0530

    Added improving validation UX

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -17,13 +17,13 @@ const validate = values => {
     let errors = {}
 
     if (!values.name) {
-        errors.name = "Required";
+        errors.name = "This Field is Required";
     } if (!values.email) {
-        errors.email = "Required";
+        errors.email = "This Field is Required";
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = "Invalid email address";
     } if (!values.channel) {
-        errors.channel = "Required";
+        errors.channel = "This Field is Required";
     }
     return errors;
 }
@@ -36,18 +36,49 @@ const SimpleForm = () => {
     })
 
     // console.log("Form Values",formik.values);
-    console.log("Form Errors",formik.errors);
+    // console.log("Form Errors",formik.errors);
+    console.log("Visited Field",formik.touched);
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name}/>
-        <label htmlFor='email'>E-mail</label>
-              <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
-        <label htmlFor='channel'>Channel</label>
-              <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
-              <button type='submit' className='form-control-btn'>Submit</button>
+        <div className='form-control'>
+            <label htmlFor='name'>Name</label>
+            <input
+                type="text" 
+                id="name" 
+                name="name" 
+                onChange={formik.handleChange} 
+                value={formik.values.name} 
+                onBlur={formik.handleBlur}
+            />
+            {formik.touched.name && formik.errors.name?<div className='error'>{formik.errors.name}</div>:null}
+        </div>
+        <div className='form-control'>
+            <label htmlFor='email'>E-mail</label>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                onChange={formik.handleChange} 
+                value={formik.values.email}
+                onBlur={formik.handleBlur}
+            />
+            {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+        </div>
+        <div className='form-control'>
+            <label htmlFor='channel'>Channel</label>
+            <input
+                type="text" 
+                id="channel" 
+                name="channel" 
+                onChange={formik.handleChange} 
+                value={formik.values.channel}
+                onBlur={formik.handleBlur}
+            />
+            {formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+        </div>
+        <button type='submit' className='form-control-btn'>Submit</button>
       </form>
     </div>
   )
