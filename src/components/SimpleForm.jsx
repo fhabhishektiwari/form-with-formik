f826a88d9ae19c93f85af385578dd091d76face4
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-02-07 17:13:00 +0530

    added yup

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 const initialValues = {
     name: "Abhishek",
@@ -28,11 +29,18 @@ const validate = values => {
     return errors;
 }
 
+const validationSchema=Yup.object({
+    name:Yup.string().required("Required!"),
+    email:Yup.string().email('Invalid email format').required('Required'),
+    channel:Yup.string().required("Required"),
+})
+
 const SimpleForm = () => {
     const formik=useFormik({
         initialValues,
         onSubmit,
-        validate,
+        // validate,
+        validationSchema,
     })
 
     // console.log("Form Values",formik.values);
